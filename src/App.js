6d9d35610d09ca6f5868498d75fa74f436da0697
Shubham-Shrivastav <shuboom@969@gmail.com>
2023-05-09 15:46:46 +0530

    added css and basic debugging to it tryed to optimized the code and added a upscreen for input username

--- src/App.js
+++ src/App.js
@@ -2,35 +2,50 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('dummyUser');
-  const [chats, setChats] = useState([{ name: 'user1', message: 'message1' }, { name: 'dummyUser', message: 'message2' }]);
+  const [name, setName] = useState('');
+  const [chats, setChats] = useState([]);
 
   const [msg, setMsg] = useState('');
 
   const sendChat = () => {
     const c = [...chats];
-    c.push({ name, message: msg });
+    c.push({ name: name, message: msg });
     setChats(c);
     setMsg('');
-  }
+  };
+
   return (
     <div>
-      <h3>User: {name}</h3>
-      <div className="chat-container">
-        {chats.map(c => <div className={`container ${c.name === name ? 'me' : ''}`}>
-          <p className="chatbox">
-            <strong>{c.name}:</strong>
-            <span> {c.message}</span>
-          </p>
-        </div>)}
-      </div>
+      {name ? null : <div>
+        <input tpe="text" placeholder="Enter your name to chat" onBlur={e => setName(e.target.value)}></input>
+      </div>}
 
-      <div className="btm">
-        <input type="text" placeholder="enter your message" onInput={e => setMsg(e.target.value)} value={msg}>
+      {name ? <div>
+        <h3>User: {name}</h3>
+        <div className="chat-container">
+          {chats.map((c, index) => (
+            <div
+              key={index}
+              className={`container ${c.name === name ? 'me' : ''}`}
+            >
+              <p className="chatbox">
+                <strong>{c.name}:</strong>
+                <span> {c.message}</span>
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="btm">
+          <input
+            type="text"
+            placeholder="enter your message"
+            onChange={(e) => setMsg(e.target.value)}
+            value={msg}
+          />
+          <button onClick={(e) => sendChat()}>Send</button>
+        </div>
 
-        </input>
-        <button onClick={e => sendChat()}>Send</button>
-      </div>
+      </div> : null}
 
     </div>
   );
